e6deb0e9153c45aa905314a7992a822f75a1b99c
刘耀澳 <yaoao.liu@dragonflow.com>
2017-10-18 17:49:01 +0800

    updates itsm project

--- itsm_polymer/config.js
+++ itsm_polymer/config.js
@@ -1,6 +1,8 @@
 Window.AppConfig = {
   appRoute: "",
-  // serverUrl: "",
+  // serverUrl: "http://192.168.9.62:8080"
   // serverUrl: "http://matt.siteview.com",
-  serverUrl: "http://newitsm.siteview.com"
+  serverUrl: "http://newitsm.siteview.com",
+  defaultUserName: "admin",
+  defaultPwd: "manage"
 }
